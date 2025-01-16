8c1bc9c53b2e38b250165a2b9a4240c2eadc2540
khaing17 <aungthetkhaing017@gmail.com>
2025-01-16 19:18:10 +0630

    changes

--- src/users/test/users.controller.spec.ts
+++ src/users/test/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../users.controller';
 import { UsersService } from '../users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -9,7 +10,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService, PrismaService],
+      providers: [UsersService, PrismaService, JwtService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
